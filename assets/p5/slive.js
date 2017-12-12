a6a3b93e8e7d18408cfa2256566631afe103d888
grvashu <grv.ashu@gmail.com>
2017-12-13 00:52:24 +0530

    source formatting

--- assets/p5/slive.js
+++ assets/p5/slive.js
@@ -11,83 +11,82 @@ var board;
 var next;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  w = 8;
-  columns = floor(width/w);
-  rows = floor(height/w);
-  board = new Array(columns);
-  for (var i = 0; i < columns; i++) {
-    board[i] = new Array(rows);
-  } 
-  next = new Array(columns);
-  for (i = 0; i < columns; i++) {
-    next[i] = new Array(rows);
-  }
-  init();
- frameRate(8);
+	createCanvas(windowWidth, windowHeight);
+	w = 8;
+	columns = floor(width / w);
+	rows = floor(height / w);
+	board = new Array(columns);
+	for (var i = 0; i < columns; i++) {
+		board[i] = new Array(rows);
+	}
+	next = new Array(columns);
+	for (i = 0; i < columns; i++) {
+		next[i] = new Array(rows);
+	}
+	init();
+	frameRate(8);
 	noStroke();
 }
 
 function draw() {
-  //background(255);
-  generate();
-  for ( var i = 0; i < columns;i++) {
-    for ( var j = 0; j < rows;j++) {
-      if ((board[i][j] == 1)){
-//		  var rnd = random(4);
-//		  if(rnd<1){ 
-//			  fill('#ebdbe3');
-//			  //fill(255,230,248);
-//		  }else if(rnd<2){
-//			  fill('#e1e3e8');
-//			  //fill(225,244,255);
-//		  }else{
-//			  fill('#fdeedb');
-//			  //fill(255,238,225);
-//		  }
-		  fill(245,245,245);
-	  }
-      else fill(255,100); 
-      //stroke(0,0);
-      rect(i*w, j*w, w-1, w-1);
-    }
-  }
+	//background(255);
+	generate();
+	for (var i = 0; i < columns; i++) {
+		for (var j = 0; j < rows; j++) {
+			if ((board[i][j] == 1)) {
+				//		  var rnd = random(4);
+				//		  if(rnd<1){ 
+				//			  fill('#ebdbe3');
+				//			  //fill(255,230,248);
+				//		  }else if(rnd<2){
+				//			  fill('#e1e3e8');
+				//			  //fill(225,244,255);
+				//		  }else{
+				//			  fill('#fdeedb');
+				//			  //fill(255,238,225);
+				//		  }
+				fill(245, 245, 245);
+			} else fill(255, 100);
+			//stroke(0,0);
+			rect(i * w, j * w, w - 1, w - 1);
+		}
+	}
 
 }
 
 function mousePressed() {
-  init();
+	init();
 }
 
 function init() {
-  for (var i = 0; i < columns; i++) {
-    for (var j = 0; j < rows; j++) {
-      if (i == 0 || j == 0 || i == columns-1 || j == rows-1) board[i][j] = 0;
-      else board[i][j] = floor(random(2));
-      next[i][j] = 0;
-    }
-  }
+	for (var i = 0; i < columns; i++) {
+		for (var j = 0; j < rows; j++) {
+			if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1) board[i][j] = 0;
+			else board[i][j] = floor(random(2));
+			next[i][j] = 0;
+		}
+	}
 }
 
 function generate() {
 
-  for (var x = 1; x < columns - 1; x++) {
-    for (var y = 1; y < rows - 1; y++) {
-      var neighbors = 0;
-      for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-          neighbors += board[x+i][y+j];
-        }
-      }
-      neighbors -= board[x][y];
-      if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;           // Loneliness
-      else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;           // Overpopulation
-      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
-      else                                             next[x][y] = board[x][y]; // Stasis
-    }
-  }
+	for (var x = 1; x < columns - 1; x++) {
+		for (var y = 1; y < rows - 1; y++) {
+			var neighbors = 0;
+			for (var i = -1; i <= 1; i++) {
+				for (var j = -1; j <= 1; j++) {
+					neighbors += board[x + i][y + j];
+				}
+			}
+			neighbors -= board[x][y];
+			if ((board[x][y] == 1) && (neighbors < 2)) next[x][y] = 0; // Loneliness
+			else if ((board[x][y] == 1) && (neighbors > 3)) next[x][y] = 0; // Overpopulation
+			else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1; // Reproduction
+			else next[x][y] = board[x][y]; // Stasis
+		}
+	}
 
-  var temp = board;
-  board = next;
-  next = temp;
+	var temp = board;
+	board = next;
+	next = temp;
 }
